e8ac622d5d2bd3d8d224a62dfa9245177705084f
Ganesh <veeraganesh10263@gmail.com>
2022-06-18 13:37:47 +0530

    commit after adding new files

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__=="__main__":
+    app.run(debug=True,port=8000)
