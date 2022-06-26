f7f21fe9926af8bf9f1bd9f92cf65390aad78194
Diswar <mungandadeswar@gmail.com>
2022-06-26 14:55:54 +0530

    tried to connect backend using flask_mysqldb

--- delivery.py
+++ delivery.py
@@ -1,18 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, redirect,  render_template, request, url_for
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='1234'
+app.config['MYSQL_DB']='delivery'
+
+mysql=MySQL(app)
+
+@app.route("/",methods=['POST','GET'])
+@app.route("/home",methods=['POST','GET'])
+@app.route("/index",methods=['POST','GET'])
 def welcome():
-    return render_template("welcome.html")
+    if request.method=='POST':
+        details=request.form
+        first=details["First"]
+        last=details["Last"]
+        phone=details["Phone"]
+        email=details["Email"]
+        password=details["Pass"]
+        cur=mysql.connection.cursor()
+        cur.execute("INSERT INTO register(First,Last,Phone,Email,Password) VALUES(%s,%s,%s,%s,%s)",(first,last,phone,email,password))
+        mysql.connection.commit()
+        return redirect(url_for('welcome'))
+    else:
+        return render_template('welcome.html')
+    
 
-@app.route("/login")
+@app.route("/login",methods = ['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    msg=''
+    if request.method=='POST':
+        details=request.form
+        email=details["login"]
+        password=details["Pass"]
+        cur=mysql.connection.cursor()
+        result=cur.execute("Select * from register where email='%s'",(email))
+        user=result.fetchone()
+        if user:
+            if email==user['Email']:
+                if password==user['Password']:
+                    return redirect(url_for('v-nv'))
+                else:
+                    return render_template('login.html',msg='p')
+            else:
+                return render_template('login.html',msg='user')
+        else:
+            return render_template('login.html',msg='err')
+    else:
+        return render_template('login.html')
 
-@app.route("/v-nv")
+@app.route("/v-nv",methods = ['POST', 'GET'])
 def v_nv():
-    return render_template("v-nv.html")
+            return render_template('v-nv.html')
 
 @app.route("/register")
 def register():
